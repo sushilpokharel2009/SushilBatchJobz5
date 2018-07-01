a3527c22698e51677264e50e9390b1be1e1f3f5b
Shubhashree Pokharel <Shubhashree11@shubhashreesmbp.home>
2018-07-01 18:31:23 -0500

    first commit

---
+++ src/main/java/com/uvk/model/Vendor.java
@@ -0,0 +1,80 @@
+package com.uvk.model;
+
+public class Vendor {
+	
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String address;
+	private String email;
+	private String checkNumber;
+	private String accountNumber;
+	private String routingNumber;
+	private String tableName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCheckNumber() {
+		return checkNumber;
+	}
+	public void setCheckNumber(String checkNumber) {
+		this.checkNumber = checkNumber;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getRoutingNumber() {
+		return routingNumber;
+	}
+	public void setRoutingNumber(String routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	@Override
+	public String toString() {
+		return "Vendor [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", address=" + address
+				+ ", email=" + email + ", checkNumber=" + checkNumber + ", accountNumber=" + accountNumber
+				+ ", routingNumber=" + routingNumber + ", tableName=" + tableName + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
